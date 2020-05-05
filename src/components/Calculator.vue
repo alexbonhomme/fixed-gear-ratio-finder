ade36831a7a4c3e3970af67501cf1b010628b0c8
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2020-05-05 15:41:33 +0200

    feat: impl variations display

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,15 +1,46 @@
 <template>
   <div>
-    <div
-      v-for="(value, index) in variations"
-      :key="index"
-    >
-      T1: {{ value.t1 }} | T2: {{ value.t1 }} | Links: {{ value.links }} | Tension: {{ value.tension }}
-    </div>
+    <el-row :gutter="20">
+      <el-col :span="4">
+        <el-input
+          placeholder="Chainstay length"
+          v-model="chainstay"
+        ></el-input>
+      </el-col>
+      <el-col :span="20">
+        <el-button
+          :disabled="!chainstay"
+          @click="computeVariations()"
+        >
+          Compute configurations
+        </el-button>
+      </el-col>
+    </el-row>
 
-    <button @click="computeVariations()">
-      Update
-    </button>
+    <el-table
+      :data="variations"
+      stripe
+      style="width: 100%"
+    >
+      <el-table-column
+        prop="t1"
+        label="Front gear"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="t2"
+        label="Rear gear"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="links"
+        label="Links">
+      </el-table-column>
+      <el-table-column
+        prop="tension"
+        label="Tension indicator (lower is better)">
+      </el-table-column>
+    </el-table>
   </div>
 
 </template>
@@ -18,14 +49,15 @@
 import { compute } from '@/helpers/engine'
 
 export default {
-  props: {
-    chainstay: {
-      type: Number,
-      required: true
-    }
-  },
+  // props: {
+  //   chainstay: {
+  //     type: Number,
+  //     required: true
+  //   }
+  // },
   data() {
     return {
+      chainstay: undefined,
       variations: []
     }
   },
