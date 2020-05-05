8502831b9681c6d2129a948cf20fdf5f58452093
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2020-05-05 15:07:48 +0200

    feat: impl basic variation display

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <calculator :chainstay="40.5"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculator from './components/Calculator.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Calculator
   }
 }
 </script>
@@ -21,8 +20,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div
+      v-for="(value, index) in variations"
+      :key="index"
+    >
+      T1: {{ value.t1 }} | T2: {{ value.t1 }} | Links: {{ value.links }} | Tension: {{ value.tension }}
+    </div>
+
+    <button @click="computeVariations()">
+      Update
+    </button>
+  </div>
+
+</template>
+
+<script>
+import { compute } from '@/helpers/engine'
+
+export default {
+  props: {
+    chainstay: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      variations: []
+    }
+  },
+  methods: {
+    computeVariations() {
+      this.variations = compute(this.chainstay)
+
+      console.debug(this.variations)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
